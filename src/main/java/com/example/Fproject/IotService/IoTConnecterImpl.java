002fc11cf282ad39943020f638fce97e93084223
孫昕妤 <92681222+sinyuuuuu@users.noreply.github.com>
2023-05-09 14:52:13 +0800

    Add files via upload

--- src/main/java/com/example/Fproject/IotService/IoTConnecterImpl.java
+++ src/main/java/com/example/Fproject/IotService/IoTConnecterImpl.java
@@ -1,23 +1,53 @@
-package com.example.Fproject.IotService;
+package IoT;
 
-public class IoTConnecterImpl implements IoTConnecter{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-	@Override
-	public String powerOn(String url) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+public class IoTConnecterImpl implements IoTConnecter {
+    private static final String USER_AGENT = "Mozilla/5.0";
+    
+    @Override
+    public String powerOn(String url) {
+        String requestUrl = url + "/on";
+        return requestUrl;
+    }
 
-	@Override
-	public String powerOff(String url) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public String powerOff(String url) {
+        String requestUrl = url + "/off";
+        return sendGetRequest(requestUrl);
+    }
 
-	@Override
-	public String getState(String url) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public String getState(String url) {
+        String requestUrl = url + "/state";
+        return sendGetRequest(requestUrl);
+    }
+    
+    private String sendGetRequest(String requestUrl) {
+        StringBuilder response = new StringBuilder();
+        try {
+            URL url = new URL(requestUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent", USER_AGENT);
 
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                in.close();
+            } 
+            else {
+                response.append("GET request failed with response code: " + responseCode);
+            }
+        } 
+        //發生例外狀況時，將錯誤訊息附加在response字串後面返回，在這裡，response字串是用於存儲從IoT裝置接收到的數據或錯誤訊息。
+        catch (IOException e) {
+            response.append("Exception occurred while sending GET request: " + e.getMessage());
+        }
+        return response.toString();
+    }
 }
--- src/main/java/com/example/Fproject/IotService/IoTGatewayServiceImpl.java
+++ src/main/java/com/example/Fproject/IotService/IoTGatewayServiceImpl.java
@@ -1,45 +1,51 @@
-package com.example.Fproject.IotService;
+package IoT;
+
+//import java.util.UUID;
 
 public class IoTGatewayServiceImpl implements IoTGatewayService {
+    private DatabaseService databaseService;
+    private IoTConnecter ioTConnecter;
 
-	@Override
-	public String powerOn(String key, String id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public IoTGatewayServiceImpl(DatabaseService databaseService, IoTConnecter ioTConnecter) {
+        this.databaseService = databaseService;
+        this.ioTConnecter = ioTConnecter;
+    }
 
-	@Override
-	public String powerOff(String key, String id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public String powerOn(String key, String id) {
+        if(!databaseService.authorization(key, id)) return null;
+        return ioTConnecter.powerOn(databaseService.getUrl(id));
+    }
 
-	@Override
-	public String getState(String key, String id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public String powerOff(String key, String id) {
+        if(!databaseService.authorization(key, id)) return null;
+        return ioTConnecter.powerOff(databaseService.getUrl(id));
+    }
 
-	@Override
-	public String[] addDevice(String url) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public String getState(String key, String id) {
+        if(!databaseService.authorization(key, id)) return null;
+        return ioTConnecter.getState(databaseService.getUrl(id));
+    }
 
-	@Override
-	public boolean alterDevice(String key, String id, String url) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public String[] addDevice(String url) {
+        String[] response = new String[1];
+        //response[0] = UUID.randomUUID().toString();
+        response[0] = databaseService.addDevice(url)[0];
+        return response;
+    }
 
-	@Override
-	public boolean deleteDevice(String key, String id) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public boolean alterDevice(String key, String id, String url) {
+        if(!databaseService.authorization(key, id)) return false;
+        return databaseService.alterDevice(url);
+    }
 
-	
-
-	
-
+    @Override
+    public boolean deleteDevice(String key, String id) {
+        if(!databaseService.authorization(key, id)) return false;
+        return databaseService.deleteDevice(key, id);
+    }
 }
