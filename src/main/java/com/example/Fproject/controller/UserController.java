43ea3013dec4017b89c176427a9da76fe3f08fc5
“Frank <a910413frank@gmail.com>
2023-06-23 19:19:27 +0800

    6/23 Swagger rename

--- src/main/java/com/example/Fproject/controller/UserController.java
+++ src/main/java/com/example/Fproject/controller/UserController.java
@@ -29,13 +29,13 @@ public class UserController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @Operation(summary = "Authorization", description = "Authorized users can control specific devices")
-    @RequestMapping(value="device/author", method=RequestMethod.POST)
+    @RequestMapping(value="/user/author", method=RequestMethod.POST)
     public void authorUser(){
 
     }
     @ResponseStatus(HttpStatus.CREATED)
     @Operation(summary = "add new user", description = "Add a user with authentication")
-    @RequestMapping(value="register/user", method=RequestMethod.POST)
+    @RequestMapping(value="/user/register", method=RequestMethod.POST)
     public void registerUser(){
 
     }
