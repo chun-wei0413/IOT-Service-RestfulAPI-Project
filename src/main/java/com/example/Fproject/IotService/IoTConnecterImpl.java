a10bedb67549084a10045929b7aae6ba481e5ae1
Sinyu <cindy91031123@gmail.com>
2023-06-27 00:05:37 +0800

    6.26.2

--- src/main/java/com/example/Fproject/IotService/IoTConnecterImpl.java
+++ src/main/java/com/example/Fproject/IotService/IoTConnecterImpl.java
@@ -1,62 +1,39 @@
 package com.example.Fproject.IotService;
-import com.example.Fproject.database.DatabaseService;
-import com.example.Fproject.IotService.IoTGatewayService;
-import com.example.Fproject.IotService.IoTGatewayServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.pi4j.io.gpio.*;
+//import com.pi4j.io.gpio.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.core.JsonProcessingException;
+//import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 @Service
 public class IoTConnecterImpl implements IoTConnecter {
-    @Autowired
-    private DatabaseService databaseService;
-    private IoTGatewayService ioTGatewayService;
-    private final GpioController gpio;
-    private final GpioPinDigitalOutput led;
 
-    public IoTConnecterImpl(DatabaseService databaseService,IoTGatewayService ioTGatewayService) {
-        this.databaseService = databaseService;
-        this.ioTGatewayService = ioTGatewayService;
-        gpio = GpioFactory.getInstance();
-        //Pin ledPin = DatabaseService.pin;
-        Pin ledPin = RaspiPin.GPIO_18;
-        led = gpio.provisionDigitalOutputPin(ledPin, "LED", PinState.LOW);
-    }
 
     @Override
     public String powerOn(String url) {
         String requestUrl = url + "/on";
-        led.high(); // 開啟LED燈
-        return sendGetRequest(requestUrl, "successful");
+        return sendGetRequest(requestUrl);
     }
 
     @Override
     public String powerOff(String url) {
         String requestUrl = url + "/off";
-        led.low(); // 關閉LED燈
-        return sendGetRequest(requestUrl, "successful");
+        return sendGetRequest(requestUrl);
     }
 
     @Override
     public String getState(String url) {
         String requestUrl = url + "/state";
-        if (led.isHigh()) {
-            return sendGetRequest(requestUrl, "on");
-        }
-        else {
-            return sendGetRequest(requestUrl, "off");
-        }
+        return sendGetRequest(requestUrl);
     }
 
-    private String sendGetRequest(String requestUrl, String state) {
+    private String sendGetRequest(String requestUrl) {
         StringBuilder response = new StringBuilder();
         try {
             URL url = new URL(requestUrl);
@@ -75,26 +52,8 @@ public class IoTConnecterImpl implements IoTConnecter {
         catch (IOException e) {
             response.append("Exception occurred while sending GET request: " + e.getMessage());
         }
-        // 建立回應的 JSON 字串
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            response.append(objectMapper.writeValueAsString(new ApiResponse(state)));
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
 
         return response.toString();
-    }
-
-    // 在程式結束時釋放 GPIO 資源
-    //public void shutdown() {gpio.shutdown();}
-
-    // 具有回應結果的 API 回應類別
-    private static class ApiResponse {
-        private String state;
-        public ApiResponse(String state) {
-            this.state = state;
-        }
     }
 }
 
--- src/main/java/com/example/Fproject/IotService/IoTGatewayServiceImpl.java
+++ src/main/java/com/example/Fproject/IotService/IoTGatewayServiceImpl.java
@@ -12,6 +12,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 @Service
 public class IoTGatewayServiceImpl implements IoTGatewayService {
     @Autowired
@@ -27,36 +33,39 @@ public class IoTGatewayServiceImpl implements IoTGatewayService {
 
     @Override
     public String powerOn(String userId, String deviceId, String password) {
-        databaseService.authorization(userId,deviceId,password);
-        //databaseService.authorization(deviceId,password);
-        string url = databaseService.getUrl(deviceId);
+        databaseService.authorization(userId,password);
+        databaseService.authentication(deviceId,password);
+        String url = databaseService.getUrl(deviceId);
         ioTConnecter.powerOn(url);
-        return "";
+        String requestUrl = url + "/on";
+        return sendGetRequest(requestUrl);
     }
 
     @Override
     public String powerOff(String userId, String deviceId, String password) {
-        databaseService.authorization(userId,deviceId,password);
-            String url = device(url);
-            return url;
-        }
-        else return "fall";
+        databaseService.authorization(userId,password);
+        databaseService.authentication(deviceId,password);
+        String url = databaseService.getUrl(deviceId);
+        ioTConnecter.powerOn(url);
+        String requestUrl = url + "/off";
+        return sendGetRequest(requestUrl);
     }
 
     @Override
     public String getState(String userId, String deviceId, String password) {
-        if(databaseService.authorization(userId,deviceId,password)) {
-            String url = device(url);
-            return url;
-        }
-        else return "fall";
+        databaseService.authorization(userId,password);
+        databaseService.authentication(deviceId,password);
+        String url = databaseService.getUrl(deviceId);
+        ioTConnecter.powerOn(url);
+        String requestUrl = url + "/state";
+        return sendGetRequest(requestUrl);
     }
 
     @Override
-    public String[] addDevice(String url,String type,String pin,String manager) {
-        String[] response = new String[1];
-        response[0] = databaseService.addDevice(url, type, pin, manager)[0];
-        return response;
+    public boolean addDevice(String url,String type,String pin,String manager) {
+
+        databaseService.addDevice(url, type, pin, manager);
+        return true;
     }
 
     @Override
@@ -69,5 +78,27 @@ public class IoTGatewayServiceImpl implements IoTGatewayService {
     public boolean deleteDevice(String key,String id) {
         if(!databaseService.authorization(key, id)) return false;
         return databaseService.deleteDevice(id);
+    }
+    private String sendGetRequest(String requestUrl) {
+        StringBuilder response = new StringBuilder();
+        try {
+            URL url = new URL(requestUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                in.close();
+            }
+            else {
+                response.append("GET request failed with response code: " + responseCode);
+            }
+        }
+        catch (IOException e) {
+            response.append("Exception occurred while sending GET request: " + e.getMessage());
+        }
+
+        return response.toString();
     }
 }
